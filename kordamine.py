f5867fb1218f345257938ab96dbd40b9c5534503
Katriin <katriin.poolakese@khk.ee>
2020-09-23 13:30:41 +0300

    Olemas

--- kordamine.py
+++ kordamine.py
@@ -8,14 +8,13 @@ class Inimene:
         self.kaal = kaal
         self.pikkus = pikkus
 
-
     def kehamassiindex(self):
         kehamass = round(self.kaal//((self.pikkus*0.01)**2))
         return kehamass
 
+
 try:
     Mari = Inimene(60, 150)
     print("Mari kehamassiindex on: "+str(Mari.kehamassiindex()))
-except:
-    Viga(Exception)
+except Viga(Exception):
     print("Midagi läks valesti!")
