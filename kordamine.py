97648b7ea930e5060a9f7ea5f5e97830ff8737f6
Katriin <katriin.poolakese@khk.ee>
2020-09-23 10:02:58 +0300

    done

---
+++ kordamine.py
@@ -0,0 +1,21 @@
+class Viga(Exception):
+    pass
+
+
+class Inimene:
+
+    def __init__(self, kaal, pikkus):
+        self.kaal = kaal
+        self.pikkus = pikkus
+
+
+    def kehamassiindex(self):
+        kehamass = round(self.kaal//((self.pikkus*0.01)**2))
+        return kehamass
+
+try:
+    Mari = Inimene(60, 150)
+    print("Mari kehamassiindex on: "+str(Mari.kehamassiindex()))
+except:
+    Viga(Exception)
+    print("Midagi läks valesti!")
